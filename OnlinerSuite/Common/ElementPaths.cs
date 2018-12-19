526376ac25abac72c04c6f557543708bc76d6f74
Anton Viaryha <AViaryha@itsupportme.by>
2018-12-19 19:54:45 +0300

    OnlSuite rearrangement

--- OnlinerSuite/Common/ElementPaths.cs
+++ OnlinerSuite/Common/ElementPaths.cs
@@ -6,5 +6,8 @@ namespace Automation.OnlinerSuite
         public static string loginFieldName = "//input[@placeholder='Ник или e-mail']";
         public static string loginFieldPass = "//input[@placeholder='Пароль']";
         public static string loginButtonSubmit = "//div[@class='auth-form__control auth-form__control_condensed-additional']//button[@type = 'submit']";
+        public static string searchBar = "//input[@class='fast-search__input']";
+
+        public static string firstSearchResult = "//div[@class='result__item result__item_product']//div[@class='product__title']/a[1]";
     }
 }
